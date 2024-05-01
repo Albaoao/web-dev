0b865cea0372e8b5611246f38a87e4b40d920972
Alba <118552142+Albaoao@users.noreply.github.com>
2024-05-01 19:49:11 +0500

    Add files via upload

---
+++ lab1 cpp/lab1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// 1
+/*
+int main()
+{
+    float x, y;
+    float pi = 3.14;
+    cin >> x;
+    cout << "\n";
+    y = sqrt(exp(2 * x)) - fabs(sin(pi * x / x + 2 / 3)) + 1.7;
+    cout << "y= " << y;
+    return 0;
+}
+*/
+
+// 2
+
+/*
+int main()
+{
+    float x, y;
+    cin >> x;
+    cout << "\n";
+    y = log(fabs(x - 1.5)) + sqrt(pow(x, 4)) + sqrt(exp(3 * x) + 4);
+    cout << "y= " << y;
+    return 0;
+}
+*/
+
+// 3
+
+int main()
+{
+    float x, y;
+    cin >> x;
+    cout << "\n";
+    y = tan(5 / 3) * x + log(fabs(2 * x + 5)) + 1.8;
+    cout << "y= " << y;
+    return 0;
+}
